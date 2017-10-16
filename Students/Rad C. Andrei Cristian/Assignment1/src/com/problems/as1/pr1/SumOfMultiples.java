bdd9eb6f57075da918d8f2243f8e0e69a04603f7
radandreicristian <31438959+radandreicristian@users.noreply.github.com>
2017-10-16 21:42:45 +0300

    Add files via upload

---
+++ Students/Rad C. Andrei Cristian/Assignment1/src/com/problems/as1/pr1/SumOfMultiples.java
@@ -0,0 +1,6 @@
+package com.problems.as1.pr1;
+
+public interface SumOfMultiples {
+
+	public final int UPPER_LIMIT = 1000;
+}
---
+++ Students/Rad C. Andrei Cristian/Assignment1/src/com/problems/as1/pr2/FibonacciEvenSum.java
@@ -0,0 +1,6 @@
+package com.problems.as1.pr2;
+
+public interface FibonacciEvenSum {
+
+	public int UPPER_LIMIT=4000000;
+}
