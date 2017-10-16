8c79d1c8360106bda4d9454a9bf25ede359f05c4
LucaGav <luca.gavril7@yahoo.com>
2017-10-16 23:12:47 +0300

    Re, Assignment1

--- Students/Gavril I. Luca Nichita/Assignment1/src/Fibonacci.java
+++ Students/Gavril I. Luca Nichita/Assignment1/src/Fibonacci.java
@@ -1,23 +1,30 @@
 
 public class Fibonacci {
-	static void fib(int n)
-	{ int fibo[]=new int[n+1];
-	  int sum=0;
-	fibo[0]=0;
-	fibo[1]=1;
-	for( int i=2;i<=n;i++) {
-		fibo[i]=fibo[i-1]+fibo[i-2];
-		if(fibo[i]%2==0)
-			if(fibo[i]<4000000)
-			 sum+=fibo[i];
-			else break; 
+	
+	static void fib(int n) {
 		
-			}
-	System.out.print(sum);
-}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	fib(20000);
+		int fibo[] = new int[n + 1];
+		int sum = 0;
+		fibo[0] = 0;
+		fibo[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			fibo[i] = fibo[i - 1] + fibo[i - 2];
+			if (fibo[i] % 2 == 0)
+				if (fibo[i] < 4000000)
+					sum += fibo[i];
+				else break;
+
+		}
+		
+		System.out.print(sum);
 	}
+	
+
+	public static void main(String[] args) {
+	
+		fib(20000);
+	
+	}
+	
 
 }
--- Students/Gavril I. Luca Nichita/Assignment1/src/Palindrom.java
+++ Students/Gavril I. Luca Nichita/Assignment1/src/Palindrom.java
@@ -1,27 +1,27 @@
 
 public class Palindrom {
-	
-	public static int isPal(int number)
-	{
-		int reverse=0;
-		int copy=number;
-		while(copy>0) {
-			reverse=10*reverse+ copy%10;
-			copy/=10;
+
+	public static int isPal(int number) {
+		int reverse = 0;
+		int copy = number;
+		while (copy > 0) {
+			reverse = 10 * reverse + copy % 10;
+			copy /= 10;
 		}
-		
-		if(reverse==number) return 1;
-	return 0;	
+
+		if (reverse == number) return 1;
+			
+		return 0;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		int max=100001;
-		for(int i=999; i>= 100; i--) {
-			for(int j=999; j>=100; j--) {
-				int palindromiq=i*j;
-				if(max < palindromiq && isPal(palindromiq)==1) {
-					max=palindromiq; // We need a max so we enter all loops
+
+		int max = 100001;
+		for (int i = 999; i >= 100; i--) {
+			for (int j = 999; j >= 100; j--) {
+				int palindromiq = i * j;
+				if (max < palindromiq && isPal(palindromiq) == 1) {
+					max = palindromiq; // We need a max so we enter all loops
 				}
 			}
 		}
